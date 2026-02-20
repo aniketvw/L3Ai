b218561bbf0a563c56938df9a565b4e1ac993642
Aniket Wagh <aniketvw14@gmail.com>
2026-02-20 18:04:04 +0530

    Fix Merge.

--- shared-contracts/src/main/java/com/rcai/shared/contracts/TradeEvent.java
+++ shared-contracts/src/main/java/com/rcai/shared/contracts/TradeEvent.java
@@ -1,9 +1,10 @@
 package com.rcai.shared.contracts;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 
 import java.math.BigDecimal;
 import java.time.Instant;
